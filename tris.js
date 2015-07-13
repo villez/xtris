fdb8e531913594f1c911e8ef41a37b68e1adba26
Ville Siltanen <ville.siltanen@iki.fi>
2015-07-13 10:16:23 +0300

    some more initial stuff

--- tris.js
+++ tris.js
@@ -6,6 +6,7 @@
     };
 
     var state = {
+        board: null,
         moveTimer: null
     };
 
@@ -14,6 +15,19 @@
 
     ctx.fillStyle = "green";
     ctx.strokeStyle = "black";
+
+    function Board() {
+        this.elems = new Array(10 * 20);
+    }
+
+    Board.prototype.get = function(x, y) {
+        return this.elems[x + 20 * y];
+    };
+
+    Board.prototype.set = function(x, y, value) {
+        this.elems[x + 20 * y] = value;
+    };
+
 
     function Piece(x, y) {
         this.x = x;
@@ -27,19 +41,19 @@
         ctx.fillRect(this.x * config.blockSize + 1, this.y * config.blockSize + 1,
                      dx-2, dy-2);
         ctx.strokeRect(this.x * config.blockSize + 1, this.y * config.blockSize + 1,
-                     dx-2, dy-1);
+                     dx-2, dy-2);
+    };
+
+    Piece.prototype.canMoveDown = function() {
+        return this.y < 19 && !state.board.get(this.x, this.y + 1);
     };
 
     Piece.prototype.moveDown = function() {
-        if (this.y < 19) {
-            this.clearCurrentPos();
-            this.y++;
-            this.draw();
-        } else {
-            clearInterval(state.moveTimer);
-            next();
-        }
-
+        this.clearCurrentPos();
+        state.board.set(this.x, this.y, false);
+        this.y++;
+        state.board.set(this.x, this.y, true);
+        this.draw();
     };
 
     Piece.prototype.clearCurrentPos = function() {
@@ -48,15 +62,26 @@
     };
 
 
-    function next() {
-        var a = new Piece();
+    function nextPiece() {
+        var p = new Piece();
+        state.currentPiece = p;
 
-        a.x = Math.floor(Math.random() * 10);
-        a.y = 0;
-        a.draw();
-        state.moveTimer = setInterval(a.moveDown.bind(a), 200);
+        p.x = Math.floor(Math.random() * 10);
+        p.y = 0;
+        p.draw();
+        state.moveTimer = setInterval(dropPiece, 100);
     }
 
-    next();
+    function dropPiece() {
+        if (state.currentPiece.canMoveDown()) {
+            state.currentPiece.moveDown();
+        } else {
+            clearInterval(state.moveTimer);
+            nextPiece();
+        }
+    }
+
+    state.board = new Board();
+    nextPiece();
 
 }());
